5bf52038159ffdcfcc1321130f8dd8d13c55c746
Daniel Gomes Mateus <daniel.analista@outlook.com>
2018-10-23 19:30:37 -0300

    Adicionado a função de extração

--- src/controllers/ImagesController.js
+++ src/controllers/ImagesController.js
@@ -1,11 +1,11 @@
 module.exports.index = (app, request, response) => {
 
-    let model = new app.src.models.ImagesModel();
-    model.index(app, request, response);
+    let model = new app.src.models.ImagesModel(app, request, response);
+    model.index();
 };
 
 module.exports.add = (app, request, response) => {
 
-  let model = new app.src.models.ImagesModel();
-  model.add(app, request, response);
+  let model = new app.src.models.ImagesModel(app, request, response);
+  model.add();
 };
--- src/models/ImagesModel.js
+++ src/models/ImagesModel.js
@@ -1,35 +1,55 @@
 class ImagesModel {
 
-  constructor() {
+  constructor(app, request, response) {
 
+    this.app      = app;
+    this.request  = request;
+    this.response = response;
   }
 
-  index(app, request, response) {
+  index() {
 
     const fs   = require('fs');
     const path = require('path');
     const face = require('face-recognition');
 
-    const dataPath   = path.resolve('src/public/images/');
+    const dataPath   = path.resolve('src/public/images/pattern/');
     const classNames = ['sheldon', 'lennard', 'raj', 'howard', 'stuart'];
 
     const allFiles      = fs.readdirSync(dataPath);
     const imagesByClass = classNames.map(
       name =>
         allFiles
-        .filter(file => name.includes(name))
-        .map(file => path.join(dataPath, file))
-        .map(end => face.loadImage(end))
+        .filter(file => name.includes(name))  // Verifica se existe alguma imagem com o mesmo nome do array
+        .map(file => path.join(dataPath, file)) // Cria o caminho com os delimitadores
+        .map(end => face.loadImage(end)) // Carrega a imagem
     );
 
     const numTrainingFaces = 10;
     const trainDataByClass = imagesByClass.map(imgs => imgs.slice(0, numTrainingFaces));
     const testeDataByClass = imagesByClass.map(imgs => imgs.slice(numTrainingFaces));
+
+    this.extract();
   };
 
-  add(app, request, response) {
+  add() {
 
-    response.send('Envio de imagens para reconhecimento.');
+    this.response.send('Envio de imagens para reconhecimento.');
+  };
+
+  extract() {
+
+    const face       = require('face-recognition');
+    const path       = require('path');
+    const image      = face.loadImage(path.resolve('src/public/images/pattern/users.jpeg'));
+    const detector   = face.FaceDetector();
+    const targetSize = 150;
+    const faceImages = detector.detectFaces(image, targetSize);
+
+    faceImages.forEach((picture, i) => {
+
+      face.saveImage(path.resolve(`src/public/images/updated/face_${i}.png`), picture);
+    });
   };
 }
 
