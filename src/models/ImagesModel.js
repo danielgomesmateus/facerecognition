e8c2628b9883ec885f959cc57a369ee3c208b402
Daniel Gomes Mateus <daniel.analista@outlook.com>
2018-10-23 18:35:48 -0300

    Alterando estrutura model ImagesModel

--- src/models/ImagesModel.js
+++ src/models/ImagesModel.js
@@ -1,33 +1,37 @@
-function ImagesModel() {
+class ImagesModel {
+
+  constructor() {
+
+  }
+
+  index(app, request, response) {
+
+    const fs   = require('fs');
+    const path = require('path');
+    const face = require('face-recognition');
+
+    const dataPath   = path.resolve('src/public/images/');
+    const classNames = ['sheldon', 'lennard', 'raj', 'howard', 'stuart'];
+
+    const allFiles      = fs.readdirSync(dataPath);
+    const imagesByClass = classNames.map(
+      name =>
+        allFiles
+        .filter(file => name.includes(name))
+        .map(file => path.join(dataPath, file))
+        .map(end => face.loadImage(end))
+    );
+
+    const numTrainingFaces = 10;
+    const trainDataByClass = imagesByClass.map(imgs => imgs.slice(0, numTrainingFaces));
+    const testeDataByClass = imagesByClass.map(imgs => imgs.slice(numTrainingFaces));
+  };
+
+  add(app, request, response) {
+
+    response.send('Envio de imagens para reconhecimento.');
+  };
 }
-
-ImagesModel.prototype.index = (app, request, response) => {
-
-  const fs   = require('fs');
-  const path = require('path');
-  const face = require('face-recognition');
-
-  const dataPath   = path.resolve('src/public/images/');
-  const classNames = ['sheldon', 'lennard', 'raj', 'howard', 'stuart'];
-
-  const allFiles      = fs.readdirSync(dataPath);
-  const imagesByClass = classNames.map(
-    name =>
-      allFiles
-      .filter(file => name.includes(name))
-      .map(file => path.join(dataPath, file))
-      .map(end => face.loadImage(end))
-  );
-
-  const numTrainingFaces = 10;
-  const trainDataByClass = imagesByClass.map(imgs => imgs.slice(0, numTrainingFaces));
-  const testeDataByClass = imagesByClass.map(imgs => imgs.slice(numTrainingFaces));
-};
-
-ImagesModel.prototype.add = (app, request, response) => {
-
-  response.send('Envio de imagens para reconhecimento.');
-};
 
 module.exports = function() {
 
